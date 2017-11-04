d03d59a579705dfe6d93078e74cd82d2a10ba7fd
LABA18-12 <LABA18-12@LABA18-02-PC>
2017-11-04 10:50:07 -0200

    Criação das classes Eletronicos e livro

---
+++ src/br/Fib/Ecommece/Livro.java
@@ -0,0 +1,78 @@
+package br.Fib.Ecommece;
+
+public class Livro extends Produto {
+
+	public String Autor;
+	public String Editora;
+	public String ISBN;
+	public String Paginas;
+	public String Edicao;
+	public String Ano;
+	public String Assunto;
+	public String Idioma;
+
+	public String getAutor() {
+		return Autor;
+	}
+
+	public void setAutor(String autor) {
+		Autor = autor;
+	}
+
+	public String getEditora() {
+		return Editora;
+	}
+
+	public void setEditora(String editora) {
+		Editora = editora;
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+
+	public String getPaginas() {
+		return Paginas;
+	}
+
+	public void setPaginas(String paginas) {
+		Paginas = paginas;
+	}
+
+	public String getEdicao() {
+		return Edicao;
+	}
+
+	public void setEdicao(String edicao) {
+		Edicao = edicao;
+	}
+
+	public String getAno() {
+		return Ano;
+	}
+
+	public void setAno(String ano) {
+		Ano = ano;
+	}
+
+	public String getAssunto() {
+		return Assunto;
+	}
+
+	public void setAssunto(String assunto) {
+		Assunto = assunto;
+	}
+
+	public String getIdioma() {
+		return Idioma;
+	}
+
+	public void setIdioma(String idioma) {
+		Idioma = idioma;
+	}
+
+}
