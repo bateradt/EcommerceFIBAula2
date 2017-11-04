21f88fe1b05ee8fdd5a3cbc38d52566d818784a5
Helder Traci <helderdiin.work@gmail.com>
2017-11-04 10:52:01 -0200

    teste

---
+++ src/br/Fib/Ecommece/Carrinho.java
@@ -0,0 +1,23 @@
+package br.Fib.Ecommece;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Carrinho {
+	double total = 0;
+	List<Produto> itens = new ArrayList<Produto>();
+	
+	public void adicionarProduto(Produto produto) {
+		//this.total += produto.getValor();
+		this.itens.add(produto);
+	}
+	
+	public void removeProduto(Produto produto) {
+		this.itens.remove(produto);
+		//this.total += produto.getValor();
+	}
+	
+	public void finalizar() {
+		
+	}
+}
