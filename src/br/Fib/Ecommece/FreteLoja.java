0469bb64b2a6cf5e55ff48c3f1aa0ee71897f270
LABA18-12 <LABA18-12@LABA18-01-PC>
2017-11-04 11:48:34 -0200

    comentado os atributos nao usados.

--- src/br/Fib/Ecommece/FreteLoja.java
+++ src/br/Fib/Ecommece/FreteLoja.java
@@ -2,9 +2,6 @@ package br.Fib.Ecommece;
 
 public class FreteLoja extends Frete {
 
-	private double valor;
-	private int prazo;
-
 	@Override
 	public void calcularFrete(String cep) {
 		this.setValor(0);
